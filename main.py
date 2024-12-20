bd6764339cf088bdb8610fb469888b56624aec2f
Bilyashik <sad@gmail.com>
2024-12-20 19:29:36 +0700

    Создание второй функции

--- main.py
+++ main.py
@@ -31,3 +31,33 @@ def maclaurin_cos(x):
         term = ((-1)**n * x**(2*n)) / math.factorial(2*n)
         result += term
     return result
+
+def maclaurin_power(x, m):
+    """
+    Вычисляет (1 + x)^m через ряд Маклорена.
+
+    Подробное описание:
+    Используется разложение (1 + x)^m в ряд Маклорена до заданного количества итераций.
+
+    Аргументы:
+    x (float): Значение x.
+    m (float): Показатель степени m.
+
+    Возвращает:
+    float: Приблизительное значение (1 + x)^m.
+
+    Исключения:
+    ValueError: Если x или m некорректны.
+
+    Примеры использования:
+    maclaurin_power(0.5, 2)
+    2.25
+    """
+    if not isinstance(x, (int, float)) or not isinstance(m, (int, float)):
+        raise ValueError("Аргументы должны быть числами.")
+
+    result = 1
+    for n in range(1, ITERATIONS):
+        term = (math.prod([m - k + 1 for k in range(1, n + 1)]) * x**n) / math.factorial(n)
+        result += term
+    return result
